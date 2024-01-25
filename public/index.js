ff911d0888b61c578e3644f122a3b36cb4906d20
Rahul <rkpandey5001@gmail.com>
2024-01-25 00:47:28 -0800

    second

---
+++ public/index.js
@@ -0,0 +1,14 @@
+// let innernav=document.getElementsByClassName('innernav');
+
+
+let subline=document.getElementsByClassName('subline');
+subline[0].style="font-size: 12px;line-height: 20px;line-height: 20px;color: #9a9fbd;font-weight: 600;font-family:Open Sans";
+subline[1].style="font-size: 12px;line-height: 20px;line-height: 20px;color: #9a9fbd;font-weight: 600;font-family:Open Sans";
+console.log(subline[0]);
+
+let line1=document.getElementById('line1');
+line1.style="font-size: 30px;line-height: 40px;color: #242830;font-weight: 700;font-family: Source Sans Pro;";
+
+let line2=document.getElementById('line2');
+line2.style="margin-top:15px;font-size: 12px;line-height: 20px;color: #242830;font-weight: 600;font-family: Open Sans;";
+
